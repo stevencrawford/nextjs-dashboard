74b7d77293b26a4fcb5399a81fbb9b9a10b7fd3e
Steven Crawford <steven.crawford.uk@gmail.com>
2024-02-09 10:34:43 +0100

    chore: prettier formating all src

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -4,25 +4,25 @@ import { fetchCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Create Invoice',
+  title: 'Create Invoice',
 };
 
 export default async function Page() {
-    const customers = await fetchCustomers();
+  const customers = await fetchCustomers();
 
-    return (
-        <main>
-            <Breadcrumbs
-                breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
-                    {
-                        label: 'Create Invoice',
-                        href: '/dashboard/invoices/create',
-                        active: true,
-                    },
-                ]}
-            />
-            <Form customers={customers} />
-        </main>
-    );
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -23,57 +23,55 @@ export default function LoginForm() {
         <div className="w-full">
           <div>
             <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="email"
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
               <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email address"
-                  required
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                required
               />
-              <AtSymbolIcon
-                  className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
             <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="password"
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="password"
             >
               Password
             </label>
             <div className="relative">
               <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="password"
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  required
-                  minLength={6}
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                required
+                minLength={6}
               />
-              <KeyIcon
-                  className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
-        <LoginButton/>
+        <LoginButton />
         <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
         >
           {errorMessage && (
-              <>
-                <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
-                <p className="text-sm text-red-500">{errorMessage}</p>
-              </>
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
           )}
         </div>
       </div>
@@ -85,8 +83,8 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-      <Button className="mt-4 w-full" aria-disabled={pending}>
-        Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
-      </Button>
+    <Button className="mt-4 w-full" aria-disabled={pending}>
+      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
   );
 }
